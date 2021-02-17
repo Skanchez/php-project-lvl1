6cd4baf6c2f54c4bb0ef45cc98a6612c0a0338ea
Skanchez <lococarcajada@gmail.com>
2021-02-17 21:25:27 +0300

    new executable file - brain-calc and it`s logic

--- src/logic.php
+++ src/logic.php
@@ -7,10 +7,7 @@ use function cli\prompt;
 
 function isEven()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-
+    $name = hiThere();
     $counter = 0;
     do {
         line('Answer "yes" if the number is even, otherwise answer "no".');
@@ -28,6 +25,42 @@ function isEven()
         } else {
             line("'$answer' is wrong answer ;(. Correct answer was '$question'.");
             return line("Let`s try again, $name!");
+        }
+    } while ($counter < 3);
+    line("Congratulations, $name!");
+}
+
+function calcThis()
+{
+    $name = hiThere();
+    $counter = 0;
+    do {
+        line("What is the result of the expression?");
+        $operators = ['+', '-', '*'];
+        $first = rand(0, 50);
+        $last = rand(0, 50);
+        $index = array_rand($operators);
+        $operator = $operators[$index];
+        line("Question: $first $operator $last");
+        switch ($operator) {
+            case '+':
+                $result = $first + $last;
+                break;
+            case '-':
+                $result = $first - $last;
+                break;
+            case '*':
+                $result = $first * $last;
+                break;
+        }
+        $answer = (int)prompt("Your answer");
+        if ($answer === $result) {
+            line("Correct!");
+            $counter++;
+        } else {
+            line("'$answer' is wrong answer ;(. Correct answer was '$result'.");
+            return line("Let`s try again, $name!");
+            break;
         }
     } while ($counter < 3);
     line("Congratulations, $name!");
